9a28178b49635c413d9e49e60a7707a0fb62b671
Phil Chaplin <pchapl1@gmail.com>
2026-05-27 19:26:04 -0700

    added registration and login

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]  # Allow anyone to access this view
--- server/config/urls.py
+++ server/config/urls.py
@@ -5,8 +5,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #login
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # refresh jwt token
+
+    path('api/auth/', include('accounts.urls')), 
 
     path('api/', include('circles.urls')),
     path('api/', include('memories.urls')),
